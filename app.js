e0ba8d8b0d126057cc2c1deaa298ba367e813902
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-10-17 20:01:10 +0200

    js varibles done

---
+++ app.js
@@ -0,0 +1,12 @@
+const cartbtn =document.querySelector('.cart-btn');
+const closeCartBtn =document.querySelector('.closer-cart')
+const clearCartBtn =document.querySelector('.clear-cart')
+const cardDom =document.querySelector('.cart')
+const cartOverlay =document.querySelector('.cart-overlay')
+const cartItems =document.querySelector('.cart-items')
+const cartTotal =document.querySelector('.cart-total')
+const cartContent =document.querySelector('.cart-content')
+const productsDom =document.querySelector('.products-center')
+//my cart
+let cart=[];
+ 
